7fb41d1038750b2d1199cb5cb467e88a12d8585f
Pawan <pawanbajaj2012@gmail.com>
2023-01-24 22:50:45 +0530

    get

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let v = vec![1,2,3,4,5];
+    let third = &v[2];
+
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
+
+
 }
