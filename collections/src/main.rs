a09bdb5f7ecbd85d1ee7aa26a06e32f631d5ddda
Pawan <pawanbajaj2012@gmail.com>
2023-01-24 22:52:58 +0530

    loop

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,13 +1,18 @@
 fn main() {
     let v = vec![1,2,3,4,5];
-    let third = &v[2];
-
-    println!("The third element is {}", third);
-
-    match v.get(2) {
-        Some(third) => println!("The third element is {}", third),
-        None => println!("There is no third element."),
+    
+    
+    for i in &v {
+        println!("{}", i);
     }
+    
+    // let third = &v[2];
+    // println!("The third element is {}", third);
+
+    // match v.get(2) {
+    //     Some(third) => println!("The third element is {}", third),
+    //     None => println!("There is no third element."),
+    // }
 
 
 }
