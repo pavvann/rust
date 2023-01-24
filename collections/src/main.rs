4d8019162784377c97776ba1854dc6dba05fb55e
Pawan <pawanbajaj2012@gmail.com>
2023-01-24 23:41:03 +0530

    hash or_insert

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -2,21 +2,28 @@ use std::collections::HashMap;
 
 fn main() {
 
-    let blue = String::from("Blue");
-    let yellow = String::from("Yellow");
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
 
-    let mut scores = HashMap::new();
-
-    scores.insert(blue, 10);
-    scores.insert(yellow, 50 );
-
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
     
+    println!("{:?}", map);
+    
+    /*
+    // 
+    // let blue = String::from("Blue");
+    // let yellow = String::from("Yellow");
 
+    // let mut scores = HashMap::new();
 
+    // scores.insert(blue, 10);
+    // scores.insert(yellow, 50 );
+    // 
 
-
-
-
+    // 
     // let hello = String::from("नमस्ते");
 
     // // bytes
@@ -33,8 +40,9 @@ fn main() {
     // for g in hello.graphemes(true) {
     //     println!("{}", g);
     // }
+    // 
 
-
+    // 
     // enum SpreadsheetCell {
     //     Int(i32),
     //     Float(f64),
@@ -52,7 +60,9 @@ fn main() {
     
     //     _ => println!("Not an integer")
     // };
-    
+    // 
+
+    // 
     // let mut v = vec![1,2,3,4,5];
     
     // for i in &mut v {
@@ -69,6 +79,7 @@ fn main() {
     //     Some(third) => println!("The third element is {}", third),
     //     None => println!("There is no third element."),
     // }
-
+    // 
+    */
 
 }
